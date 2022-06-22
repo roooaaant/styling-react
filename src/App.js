1069cb20a9c9b70d14e1a2ce3c176e5a4f438c99
Myoungjin <brew0@naver.com>
2022-06-22 21:32:52 +0900

    Commit before yarn eject

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SassCompont from './SassCompont';
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <SassCompont />
+      </div>
+    );
+  }
+}
+
+export default App;
